df803e8a16ad2e3730ea27d1b2bc4b32e89a21d9
ATW <andrewt.woosnam@gmail.com>
2019-03-25 15:01:17 -0400

    New repo!

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+var mongoose = require("mongoose");
+const app = express()
+const port = 3000
+
+// const db = require('./db')
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/FeedMe");
+
+var schema = new mongoose.Schema({ 
+  key: Number,
+  recipeName: String,
+  imageURL: String,
+  link: String,
+  timeInMinutes: Number,
+  servings: Number,
+  ingredients: [
+    {
+      ingredName: String,
+      amount: Number,
+      units: String,
+      adjective: String,
+      notes: String
+    }
+  ]
+});
+
+var Recipes = mongoose.model('Recipes', schema);
+
+app.get('/', (req, res) => {
+  res.send("Welcome to this custom API!     Query '\\recipes' for a JSON response containing all recipe documents.")
+});
+
+app.get('/recipes', (req, res) => {
+  const query = Recipes.find({});
+  query.exec(function (err, docs) {
+    res.send(JSON.stringify(docs));
+  });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
